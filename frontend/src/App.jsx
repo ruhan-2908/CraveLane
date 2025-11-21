e3e186371ff6836bdf2488a88a887741bfb73999
ruhan-2908 <ruhankb29@gmail.com>
2025-11-21 23:19:15 +0530

    Add Footer Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import { Routes,Route } from 'react-router-dom'
+import React, { Fragment } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Cart from './pages/Cart/Cart'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
-    <div className="app">
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
