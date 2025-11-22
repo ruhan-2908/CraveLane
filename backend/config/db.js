64256901cf7c32f955476dab361606076159ba24
ruhan-2908 <ruhankb29@gmail.com>
2025-11-22 23:28:45 +0530

    Add Sample APIs

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://ruhankb29_db_user:Vg6BEifLO83ArnPn@cluster0.s72fc5n.mongodb.net/?appName=Cluster0").then(()=>{
-        console.log("MongoDB connected successfully");
-    })
-}
