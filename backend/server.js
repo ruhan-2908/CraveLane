c7dcdaf5fd1e8fd13eb00d14cf93d09d0522ade1
ruhan-2908 <ruhankb29@gmail.com>
2025-11-23 22:29:34 +0530

    Add Encryption for the User Credentials

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 connectDB();
 //app config
 const app = express();
@@ -16,10 +18,8 @@ app.use(cors());
 //api endpoints
 app.use('/api/food',foodRouter);
 app.use("/images",express.static('uploads'))
+app.use('/api/user',userRouter);
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
