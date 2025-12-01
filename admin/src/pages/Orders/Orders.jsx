f0aa0587082b7687f986cc4768cced88a85ced9b
ruhan-2908 <ruhankb29@gmail.com>
2025-12-01 23:45:24 +0530

    End Patch #8

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -6,7 +6,7 @@ import { assets } from '../../assets/assets'
 
 const Orders = () => {
   const [orders, setOrders] = useState([])
-  const url = "http://localhost:4000"
+  const url = "https://cravelane-backend.onrender.com/"
 
   const fetchAllOrders = async () => {
   try {
