fab8f2e0bdc125b911f1baf729a8bd27478f18fd
ruhan-2908 <ruhankb29@gmail.com>
2025-12-01 23:08:26 +0530

    End Patch #6

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://cravelane-frontend.onrender.com"
     try {
         const newOrder = new orderModel({
             userId: req.userId,
